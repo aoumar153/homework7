f472e68f77169352767e1513f3cd734a79916184
Abdullah Oumar <oumarab@oregonstate.edu>
2021-05-31 19:41:58 -0700

    Wrote test to check if the second condition works with the test and works. and I added the code to make ot work

--- l.py
+++ l.py
@@ -3,6 +3,9 @@ def leap(year):
     #year = input("Input the year you want to check: ")
     #print(year)
     if(int(year)% 4 == 0):
-        return 'This is a leap year'
+        if(int(year)%100 ==0):
+            return 'not a leap year'
+        else: 
+            return 'not a leap year'
     else: 
         return 'not a leap year'
