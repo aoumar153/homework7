e0e0d9c5c51ae0cca40ebee33b5ba6c9697c6c25
Abdullah Oumar <oumarab@oregonstate.edu>
2021-05-31 17:36:00 -0700

    Made chenges to the test to ensure that the print outs for the fizzbuzz were strings or characters instead of integars, couldn't think of what else to test

--- fizzbuz.py
+++ fizzbuz.py
@@ -19,4 +19,17 @@ def test_fizz(capsys):
                 s = str(i+1)
                 assert output[i] == s
                 
-
+def test_buzz(capsys):
+    fizzbuzz()
+    output = capsys.readouterr().out.split('\n')[:-1]
+    for i in output.len():
+        if((i+1) % 3 == 0) :
+            if ((i+1) % 5 == 0 ):
+                assert output[i] == 'FizzBuzz'
+            else :
+                assert output[i] == 'Fizz'
+        else: 
+            if((i+1) % 5 == 0):
+                assert output[i] == 'Buzz'
+            else:
+                assert output[i] == i
